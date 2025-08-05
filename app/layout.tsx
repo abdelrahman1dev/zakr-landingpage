b1c78daf3b0cb42938b09c34ee14fec0d045b1cb
abdelrahman mohamed <97918409+abdelrahman1dev@users.noreply.github.com>
2025-08-06 01:14:42 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,22 @@ import { LanguageProvider } from "./components/LanguageProvider";
 export const metadata: Metadata = {
   title: "zakr educational platform",
   description: "A platform for educational resources and tools",
+    openGraph: {
+    title: 'Zakr Educational Platform',
+    description: 'Smart, bilingual platform for student time management.',
+    url: 'https://zakr.vercel.app',
+    siteName: 'Zakr',
+    images: [
+      {
+        url: 'https://zakr.vercel.app/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Zakr Preview',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
